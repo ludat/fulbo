db621b21ef5c561bba896caaac2af5663d4ad188
Lucas David Traverso <lucas6246@gmail.com>
2026-03-04 20:14:11 -0300

    Only allow ratings >= 0

--- db/migrations/010_allow_any_number_for_attributes.sql
+++ db/migrations/010_allow_any_number_for_attributes.sql
@@ -1,1 +1,6 @@
 ALTER TABLE player_ratings DROP CONSTRAINT player_ratings_rating_check;
+
+ALTER TABLE player_ratings
+ADD CONSTRAINT player_ratings_rating_check CHECK (rating >= 0) NOT VALID;
+
+ALTER TABLE player_ratings VALIDATE CONSTRAINT player_ratings_rating_check;
